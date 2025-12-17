d0f10984b223531c44af59d38dc6845a32da8097
Amirulanas19 <amirul190303@gmail.com>
2025-12-17 15:35:59 +0800

    Update index.php

--- WAD_Project/index.php
+++ WAD_Project/index.php
@@ -1,20 +1,13 @@
 <?php
-// index.php - The Traffic Controller
 
-// 1. Include the Header (So every page has the logo/nav)
 include 'includes/header.php'; 
 include 'includes/navbar.php';
 
-// 2. Figure out which page the user wants
-// If ?page=xyz is set in the URL, go there. Otherwise, go to 'home'.
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
-// 3. Whitelist allowed pages (Security: prevent hacking)
 $allowed_pages = ['home', 'login', 'register', 'about', 'menu'];
 
-// 4. Load the content
 if (in_array($page, $allowed_pages)) {
-    // Check if the file actually exists
     if (file_exists("pages/$page.php")) {
         include "pages/$page.php";
     } else {
@@ -24,6 +17,6 @@ if (in_array($page, $allowed_pages)) {
     echo "<h1>404: Page not found!</h1>";
 }
 
-// 5. Include the Footer
 include 'includes/footer.php';
+
 ?>
